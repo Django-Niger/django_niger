33468662e9ae2b50c126b26e0f1b4d7b2606e48d
HAMA Barhamou <hamabarhamou@gmail.com>
2024-01-22 00:16:16 +0100

    Réorganisation du projet et intégration correcte de Wagtail
    - Réorganisé la structure du projet pour aligner avec les standards Django
    - Supprimé le dossier mysite inutile et intégré Wagtail dans l'application principale
    - Effacé et recréé la base de données SQLite pour assurer la cohérence
    - Nettoyé les dossiers de migrations des applications et effectué de nouvelles migrations
    - Réajusté le fichier settings.py pour une configuration correcte de Wagtail
    - Testé avec succès l'accès à l'interface d'administration de Wagtail (/cms/)
    - Confirmé la fonctionnalité de création et gestion de pages dans Wagtail
    - (Inclure d'autres ajustements spécifiques ou corrections apportées)
    
    Ce commit résout les problèmes de structure antérieurs du projet et établit une base solide pour le développement futur avec Django et Wagtail.

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Add these:
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [FieldPanel("intro")]
--- django_niger/settings.py
+++ django_niger/settings.py
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
     "wagtail",
     "modelcluster",
     "taggit",
+    "blog",
 ]
 
 MIDDLEWARE = [
