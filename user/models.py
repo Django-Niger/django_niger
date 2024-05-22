2fbb9032fc3564364fd69aac9ffb80cc5fa80f39
HAMA Barhamou <hamabarhamou@gmail.com>
2024-05-22 12:44:12 +0100

    Amélioration de la gestion des utilisateurs et des profils
    - Ajout d'un modèle Profile pour une gestion améliorée des rôles des utilisateurs
    - Utilisation de signaux pour créer et mettre à jour automatiquement les profils des utilisateurs
    - Personnalisation de l'interface d'administration pour le modèle User avec un inline Profile
    - Enregistrement du modèle Profile dans l'administration avec une configuration personnalisée
    
    Ces modifications permettent une gestion plus flexible et extensible des rôles et des informations de profil des utilisateurs.

--- user/models.py
+++ user/models.py
@@ -3,11 +3,20 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    pass  # Nous n'ajoutons plus de champs directement dans le modèle User
+
+
+class Profile(models.Model):
     USER_TYPE_CHOICES = (
         ("admin", "Admin"),
         ("moderator", "Moderator"),
         ("member", "Member"),
     )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_type = models.CharField(
         max_length=10, choices=USER_TYPE_CHOICES, default="member"
     )
+
+    def __str__(self):
+        return f"{self.user.username} - {self.get_user_type_display()}"
---
+++ user/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from .models import Profile
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
