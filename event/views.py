04247b2f56b57fcfdf0aa7a7819bb0384f21d9a8
HAMA Barhamou <hamabarhamou@gmail.com>
2024-05-23 15:34:16 +0100

    - Ajout de la vérification d'inscription aux événements pour afficher un message lorsqu'un utilisateur est déjà inscrit. - Affichage conditionnel de la bannière d'appel à l'action uniquement pour les utilisateurs non connectés.

--- event/views.py
+++ event/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Event, Registration
 
 
@@ -9,11 +10,10 @@ def register_to_event(request, event_id):
     event = get_object_or_404(Event, id=event_id)
     user = request.user
     if Registration.objects.filter(event=event, participant=user).exists():
-        # Gérer le cas où l'utilisateur est déjà inscrit (par exemple, afficher un message)
-        print("Deja inscrit")
-        pass
+        messages.warning(request, "Vous êtes déjà inscrit à cet événement.")
     else:
         Registration.objects.create(event=event, participant=user, status="confirmed")
+        messages.success(request, "Votre inscription à l'événement a été confirmée.")
     return redirect("home:landing_page")
 
 
