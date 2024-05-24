cd5a6b5c636f1e6acd46f6b81a37e53e1da48ad7
HAMA Barhamou <hamabarhamou@gmail.com>
2024-05-24 21:11:02 +0100

    Djangi Niger version 1.2.0
    Corriger le middleware pour gérer le type None pour REMOTE_ADDR

--- home/middleware.py
+++ home/middleware.py
@@ -7,5 +7,8 @@ from django.utils.deprecation import MiddlewareMixin
 class VisitorMiddleware(MiddlewareMixin):
     def process_request(self, request):
         ip_address = request.META.get("REMOTE_ADDR")
-        if not Visitor.objects.filter(ip_address=ip_address).exists():
+        if (
+            not Visitor.objects.filter(ip_address=ip_address).exists()
+            and ip_address is not None
+        ):
             Visitor.objects.create(ip_address=ip_address)
